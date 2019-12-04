9301401632dcc9b28eddad14a92c8331c819976e
jod35 <jodestrevin@gmail.com>
2019-12-04 15:36:28 +0300

    switch case statements

---
+++ elseif.php
@@ -0,0 +1,24 @@
+<?php
+if (condition) {
+  // code...
+}
+elseif (condition) {          //syntax of an elseif statement
+  // code...
+}
+else {
+  // code...
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ switchstatements.php
@@ -0,0 +1,45 @@
+<?php
+//switch case statements prevent too much use of nested if statemets
+
+//e.g
+if($option==1){echo "Send Money";}
+elif($option==2){echo "Deposit Money";}
+elif($option==3){echo "Buy Airtime";}
+elif($option==4){echo "Withdraw Money";}
+elif($option==0){echo "Exit";}
+else{echo "Unknown Option";}
+
+//with a switch statement
+switch ($option){
+  case 1:
+  echo "Send Money";
+  break;
+  case 2:
+  echo "Deposit Money";
+  break;
+  case 3:
+  echo "Buy Airtime";
+  break;
+  case 0:
+  echo "Exit";
+  break;
+
+  default:
+  echo "Unknown Option";
+  break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
