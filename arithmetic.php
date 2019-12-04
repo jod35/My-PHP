1d90e251a814b9774e7febe319a19d993e2774bf
jod35 <jodestrevin@gmail.com>
2019-12-04 13:41:09 +0300

    operators

---
+++ arithmetic.php
@@ -0,0 +1,61 @@
+<?php
+//operators
+//these help us to specify the mathematical operations that we may want to carry out on data.
+
+/*
+The most common operators in php
+addition == +
+subtraction == -
+multiplication == *
+division == /
+
+modulus (remainder) == %
+
+to increment => ++$variable
+to decrement => --$variable
+
+exponential => $variable **2; (variable power 2)
+
+
+//assignment operators
+they include = , =+ ,=- ,=*, =/
+$variable =+1 ; means $variable = $variable + 1;
+
+
+//comparison operators
+these are normally used in if , for,while statements
+
+   == is equal todo
+   != is not equal
+   >   is greater than
+   <   is less than
+   <=  is less than or equal to
+   >=  is greater than or equal to
+   <>  is not equal to
+   === is identical to
+   !==  is not identical to
+
+
+//logical operators
+this help bring out meaning (logic)
+they include
+ &&, and => "and"
+ ||, or  => "or"
+ !       => "not"
+  xor    => "exclusive or"    
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+ ?>
