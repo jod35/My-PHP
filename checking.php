3d400881dd952ebee7185edeaa4245f59b93105b
jod35 <jodestrevin@gmail.com>
2019-12-04 15:56:36 +0300

    while loops and checking for existance of functions

---
+++ checking.php
@@ -0,0 +1,10 @@
+<?php
+if (function_exists("mysql_connect"))
+{
+echo "Function exists";
+}
+else
+{
+echo "Function does not exist - better write our own";
+}
+?>
---
+++ whileloops.php
@@ -0,0 +1,12 @@
+<?php
+$fuel=10;
+
+white($fuel>1)
+{
+  echo "There is enough fuel";
+}
+
+
+
+
+ ?>
