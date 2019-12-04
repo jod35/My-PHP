296f1338e299141a732a9ba5c1fc6d4a61073d97
jod35 <jodestrevin@gmail.com>
2019-12-04 13:16:33 +0300

    basics syntax and varibles

---
+++ comments.php
@@ -0,0 +1,15 @@
+<?php
+//comments make php code not to be run
+//They are mostly used to document
+//double forward slashes make single line comments
+
+//multiine comments
+
+
+/*this
+is a
+multiine
+comment
+*/
+
+ ?>
